930b99a785ed013d708766fdef8761df2d063425
Lucas Frey <lucas.frey@heyday.co.nz>
2019-11-30 19:38:37 +1300

    remove contact form

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,7 +40,7 @@ class HomeIndex extends React.Component {
                             user interfaces.</h2>
                         </header>
                         <p>From web component and responsive website to React and Javascript, check out what I have been working on recently.
-						Interested in performance optimisation, data analytics and every that can grow and improve user experience really.</p>
+                        Interested in performance optimisation, data analytics and every that can grow and improve user experience really.</p>
                     </section>
 
                     <section id="two">
@@ -52,34 +52,6 @@ class HomeIndex extends React.Component {
                             caption,
                             description
                         }))} />
-                    </section>
-
-                    <section id="three">
-                        <h2>Get In Touch</h2>
-                        <p>Here is a form if you want to contact me.</p>
-                        <div className="row">
-                            <div className="8u 12u$(small)">
-                                <form method="post" action="#">
-                                    <div className="row uniform 50%">
-                                        <div className="6u 12u$(xsmall)"><input type="text" name="name" id="name" placeholder="Name" /></div>
-                                        <div className="6u 12u$(xsmall)"><input type="email" name="email" id="email" placeholder="Email" /></div>
-                                        <div className="12u"><textarea name="message" id="message" placeholder="Message" rows="4"></textarea></div>
-                                    </div>
-                                </form>
-                                <ul className="actions">
-                                    <li><input type="submit" value="Send Message" /></li>
-                                </ul>
-                            </div>
-                            <div className="4u 12u$(small)">
-                                <ul className="labeled-icons">
-                                    <li>
-                                        <h3 className="icon fa-home"><span className="label">Address</span></h3>
-                                        Brooklyn, 6021 WELLLINGTON<br />
-                                        New Zealand
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
                     </section>
 
                 </div>
